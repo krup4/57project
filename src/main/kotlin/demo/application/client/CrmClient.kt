94cd843f9a64a5d0b20f2d3ebaa75ceea9cf9bc2
Krupnov <pavelkm10@gmail.com>
2024-12-05 23:50:32 +0300

    delete unnecessary imports

--- src/main/kotlin/demo/application/client/CrmClient.kt
+++ src/main/kotlin/demo/application/client/CrmClient.kt
@@ -1,8 +1,6 @@
 package demo.application.client
 
 import org.springframework.cloud.openfeign.FeignClient
-import org.springframework.stereotype.Component
-import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 
