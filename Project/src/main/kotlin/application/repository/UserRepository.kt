48a54b3c75d6ea2732bab2785596f35c78d4aecd
User <ilyatirex@gmail.com>
2025-02-24 23:42:58 +0300

    create repository

--- Project/src/main/kotlin/application/repository/UserRepository.kt
+++ Project/src/main/kotlin/application/repository/UserRepository.kt
@@ -1,4 +1,28 @@
 package application.repository
 
-class UserRepository {
-}
+import jakarta.persistence.*
+
+
+@Entity
+@Table(name = "files")
+class UserRepository (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(nullable = false)
+    val login: String,
+
+    @Column(nullable = false)
+    val password: String,
+
+    @Column(nullable = true)
+    val name: String? = null,
+
+    @Column(nullable = false)
+    val isAdmin: Boolean = false,
+
+    @Column(nullable = false)
+    val token: String
+)
