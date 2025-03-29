aa95510bca0c2ee28a8a410b8dfa79cb2493ca68
Krupnov <pavelkm10@gmail.com>
2025-03-29 04:46:09 +0300

    add registerAdmin page and fix backend

--- Project/src/main/kotlin/application/config/WebConfig.kt
+++ Project/src/main/kotlin/application/config/WebConfig.kt
@@ -1,4 +1,16 @@
 package application.config
 
-class WebConfig {
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:3000") // или "*" для всех
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
 }
