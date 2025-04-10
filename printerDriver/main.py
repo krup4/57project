984368f47b49b46ca40736c491f68dd4f9804987
Krupnov <pavelkm10@gmail.com>
2025-04-10 21:57:15 +0300

    rework printerDriver

--- printerDriver/main.py
+++ printerDriver/main.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, jsonify
 import os
 import subprocess
+import tempfile
+import win32print
+import win32api
+import time
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -9,6 +14,20 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'doc', 'docx', 'png', 'jpg', 'jpeg'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def is_printer_done(printer_name):
+    printer = win32print.OpenPrinter(printer_name)
+    try:
+        status = win32print.GetPrinter(printer, 2)["Status"]
+        return status == 0
+    finally:
+        win32print.ClosePrinter(printer)
+
+
+@app.route('/check', methods=['GET'])
+def check():
+    return jsonify({"status": "ok"})
 
 
 @app.route('/print', methods=['POST'])
@@ -24,18 +43,35 @@ def print_file():
     if not allowed_file(file.filename):
         return jsonify({"message": "Unsupported type of file"}), 400
 
+    file_ext = file.filename.rsplit('.', 1)[1].lower()
+
     temp_path = os.path.join("temp_uploads", file.filename)
-    file.save(temp_path)
+    with open(temp_path, 'wb') as f:
+        file.save(f)
+    time.sleep(5)
 
     try:
-        subprocess.run(['notepad', '/p', temp_path], check=True)
-        return jsonify({"message": f"File was printed"}), 200
+        printer_name = win32print.GetDefaultPrinter()
+        win32api.ShellExecute(
+            0,
+            "print",
+            temp_path,
+            f'"{printer_name}"',
+            ".",
+            0
+        )
+
+        for _ in range(30):
+            if is_printer_done(printer_name):
+                break
+            time.sleep(1)
+
+        return jsonify({"message": f"File {file.filename} was sent to printer"}), 200
     except subprocess.CalledProcessError as e:
-        return jsonify({"message": f"Error in printing"}), 500
+        return jsonify({"message": f"Error in printing: {str(e)}"}), 500
     finally:
         os.remove(temp_path)
 
 
-if __name__ == '__main__':
-    os.makedirs("temp_uploads", exist_ok=True)
-    app.run(debug=True)
+if name == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=8080)
